3b0b56d5b11f409c23debcf1e6ee1a3e229c2abb
cardosor2 <renato.cardoso@paddypowerbetfair.com>
2020-07-03 20:25:36 +0100

    Updates

---
+++ precache-manifest.09bef3162712abd4b5b664bec829926e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb79a764f2ff0be2fa884d2ccc0996ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87053e6e07e320013ac4",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d14fe5169bd0fd2df486",
+    "url": "/static/css/main.5720843c.chunk.css"
+  },
+  {
+    "revision": "87053e6e07e320013ac4",
+    "url": "/static/js/2.22798087.chunk.js"
+  },
+  {
+    "revision": "d14fe5169bd0fd2df486",
+    "url": "/static/js/main.a35000a8.chunk.js"
+  },
+  {
+    "revision": "481c3638fafd7600a182",
+    "url": "/static/js/runtime~main.273367e8.js"
+  },
+  {
+    "revision": "932b280aa8b74b3349221b9719d1794b",
+    "url": "/static/media/RenatoCardosoCV.932b280a.pdf"
+  },
+  {
+    "revision": "d6f0627ccb60b1b57b0bc3f2c321abc2",
+    "url": "/static/media/about.d6f0627c.md"
+  },
+  {
+    "revision": "36c9d2f771c258685d6df70618ad5b13",
+    "url": "/static/media/me.36c9d2f7.jpg"
+  }
+]);
